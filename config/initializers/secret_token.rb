792b15105af6e224904618942ef25cb9f8c1033d
Tomer Elmalem <telmalem@gmail.com>
2012-11-18 14:50:05 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitmadleaderboard::Application.config.secret_token = 'a61ba2c33befb446fbf985ec5abc2abd8291680b0ef1ac09ace772106adcebdcf4a378ebe1ee6aade560cbf3ee047e49723399c477dffc83ed69167bf6743e1d'
